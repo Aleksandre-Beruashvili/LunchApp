50947fe9d50975e3e01d223cdee75d05eb0a9d82
Aleksandre Beruashvili <108833484+Aleksandre-Beruashvili@users.noreply.github.com>
2025-06-09 18:51:26 +0400

    Enable Blazor server static assets

--- Server/Program.cs
+++ Server/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using LunchApp.Server.Data;
+using Microsoft.AspNetCore.Components.WebAssembly.Server;
 
 var builder = WebApplication.CreateBuilder(args);
 
