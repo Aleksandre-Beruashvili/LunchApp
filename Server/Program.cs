6ab24f8069437021c84815eef30ed7abe7b93cf9
Aleksandre Beruashvili <108833484+Aleksandre-Beruashvili@users.noreply.github.com>
2025-06-09 18:30:24 +0400

    Add missing using for Blazor server

--- Server/Program.cs
+++ Server/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Components.WebAssembly.Server;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
