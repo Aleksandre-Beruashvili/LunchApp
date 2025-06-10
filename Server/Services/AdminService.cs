8f2712c23edd1579c02ee0019614ca9f919a9012
Aleksandre Beruashvili <108833484+Aleksandre-Beruashvili@users.noreply.github.com>
2025-06-10 15:24:58 +0400

    Add missing admin service implementation

---
+++ Server/Services/AdminService.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OfficeCafeApp.API.Data;
+using OfficeCafeApp.API.Models;
+
+namespace OfficeCafeApp.API.Services
+{
+    public class AdminService : IAdminService
+    {
+        private readonly AppDbContext _context;
+
+        public AdminService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByDateAsync(DateTime date)
+        {
+            return await _context.Orders
+                .Where(o => o.OrderDate.Date == date.Date)
+                .Include(o => o.User)
+                .Include(o => o.Slot)
+                .Include(o => o.OrderItems)
+                    .ThenInclude(oi => oi.Dish)
+                .ToListAsync();
+        }
+
+        public async Task<bool> UpdateDishAvailabilityAsync(int dishId, bool isAvailable)
+        {
+            var dish = await _context.Dishes.FindAsync(dishId);
+            if (dish == null) return false;
+
+            dish.IsAvailable = isAvailable;
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
